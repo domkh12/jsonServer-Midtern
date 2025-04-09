c372ee0c25e17886f987b5625d98e16de85a697b
EI CHANUDOM <chanudomei122@gmail.com>
2025-04-09 13:19:57 +0700

    third

--- api/index.js
+++ api/index.js
@@ -1,11 +1,12 @@
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
+const path = require('path');
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router(path.join(__dirname, '../db.json'));
+const middlewares = jsonServer.defaults();
 
-server.use(middlewares)
-server.use(router)
+server.use(middlewares);
+server.use(router);
 
 module.exports = (req, res) => {
-    server(req, res)
-}
+    server(req, res);
+};
